9889b551f58d9bf1b076c2fa348ba2484b18b5f0
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-05-10 00:44:42 +0100

    Add all on / off plusy key press

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+function No_Bauble () {
+    led.plotBrightness(1, 2, Low)
+    led.plotBrightness(3, 2, Low)
+    led.plotBrightness(0, 3, Low)
+    led.plotBrightness(4, 3, Low)
+}
 function tree2 () {
     led.plotBrightness(2, 0, Low)
     led.plotBrightness(1, 1, Low)
@@ -15,8 +21,29 @@ function tree2 () {
     led.plotBrightness(3, 3, Low)
     led.plotBrightness(2, 4, Low)
 }
+input.onButtonPressed(Button.A, function () {
+    All_On()
+})
 function Star () {
     led.plotBrightness(2, 0, 255)
+}
+function All_On () {
+    Star()
+    Bauble_on()
+}
+function All_Off () {
+    No_Bauble()
+    basic.pause(100)
+    No_Star()
+}
+input.onButtonPressed(Button.B, function () {
+    All_Off()
+})
+function Bauble_on () {
+    led.plotBrightness(1, 2, 255)
+    led.plotBrightness(3, 2, 255)
+    led.plotBrightness(0, 3, 255)
+    led.plotBrightness(4, 3, 255)
 }
 function Draw_Tree_1 () {
     led.plotBrightness(2, 0, 0)
@@ -39,7 +66,11 @@ Low = 64
 Draw_Tree_1()
 basic.forever(function () {
     Star()
-    basic.pause(100)
+    basic.pause(200)
     No_Star()
+    basic.pause(500)
+    Bauble_on()
+    basic.pause(500)
+    No_Bauble()
     basic.pause(500)
 })
