954081e0d22fce5ba9904e198650962bdf1655df
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-05-10 00:02:44 +0100

    change to function to draw tree

--- main.ts
+++ main.ts
@@ -1,12 +1,16 @@
-led.setBrightness(21)
-basic.showLeds(`
-    . . . . .
-    . . # . .
-    . # # # .
-    # # # # #
-    . . # . .
-    `)
+function Draw_Tree_1 () {
+    led.plotBrightness(2, 1, 255)
+    led.plotBrightness(1, 2, 255)
+    led.plotBrightness(2, 2, 255)
+    led.plotBrightness(3, 2, 255)
+    led.plotBrightness(0, 3, 255)
+    led.plotBrightness(1, 3, 255)
+    led.plotBrightness(2, 3, 255)
+    led.plotBrightness(3, 3, 255)
+    led.plotBrightness(4, 3, 255)
+    led.plotBrightness(2, 4, 255)
+}
+Draw_Tree_1()
 basic.forever(function () {
-    led.setBrightness(255)
-    led.plotBrightness(2, 0, 255)
+	
 })
