a52d04c8cb0e88840cbda1c86f2cfab5d30c78e8
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-05-10 00:23:09 +0100

    flash star and different tree functions added

--- main.ts
+++ main.ts
@@ -1,16 +1,45 @@
-function Draw_Tree_1 () {
-    led.plotBrightness(2, 1, 255)
-    led.plotBrightness(1, 2, 255)
-    led.plotBrightness(2, 2, 255)
-    led.plotBrightness(3, 2, 255)
-    led.plotBrightness(0, 3, 255)
-    led.plotBrightness(1, 3, 255)
-    led.plotBrightness(2, 3, 255)
-    led.plotBrightness(3, 3, 255)
-    led.plotBrightness(4, 3, 255)
-    led.plotBrightness(2, 4, 255)
+function tree2 () {
+    led.plotBrightness(2, 0, Low)
+    led.plotBrightness(1, 1, Low)
+    led.plotBrightness(2, 1, Low)
+    led.plotBrightness(3, 1, Low)
+    led.plotBrightness(1, 2, Low)
+    led.plotBrightness(2, 2, Low)
+    led.plotBrightness(3, 2, Low)
+    led.plotBrightness(4, 2, Low)
+    led.plotBrightness(0, 2, Low)
+    led.plotBrightness(0, 3, Low)
+    led.plotBrightness(4, 3, Low)
+    led.plotBrightness(1, 3, Low)
+    led.plotBrightness(2, 3, Low)
+    led.plotBrightness(3, 3, Low)
+    led.plotBrightness(2, 4, Low)
 }
+function Star () {
+    led.plotBrightness(2, 0, 255)
+}
+function Draw_Tree_1 () {
+    led.plotBrightness(2, 0, 0)
+    led.plotBrightness(2, 1, Low)
+    led.plotBrightness(1, 2, Low)
+    led.plotBrightness(2, 2, Low)
+    led.plotBrightness(3, 2, Low)
+    led.plotBrightness(0, 3, Low)
+    led.plotBrightness(1, 3, Low)
+    led.plotBrightness(2, 3, Low)
+    led.plotBrightness(3, 3, Low)
+    led.plotBrightness(4, 3, Low)
+    led.plotBrightness(2, 4, Low)
+}
+function No_Star () {
+    led.plotBrightness(2, 0, 0)
+}
+let Low = 0
+Low = 64
 Draw_Tree_1()
 basic.forever(function () {
-	
+    Star()
+    basic.pause(100)
+    No_Star()
+    basic.pause(500)
 })
